25a93a645447b41c55db8133dd78dd7072ac4018
dirankunle <32834725+dirankunle@users.noreply.github.com>
2023-03-30 11:22:12 +0100

    Delete doityourslf directory

--- doityourslf/script.js
+++
@@ -1,5 +0,0 @@
-const toggleButton = document.getElementById('toggle-button');
-const naviList = document.getElementById('page-list');
-  toggleButton.addEventListener('click', () =>{
-    naviList.classList.toggle('active');
-  })
